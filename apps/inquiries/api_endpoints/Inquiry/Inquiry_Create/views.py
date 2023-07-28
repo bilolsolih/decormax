1201c8a5547873f8efcb152e23be1c9a8c3b63db
BilolSolih <BilolMuhammadSolih@gmail.com>
2023-07-28 18:39:52 +0500

    removed delay method callin of celery

--- apps/inquiries/api_endpoints/Inquiry/Inquiry_Create/views.py
+++ apps/inquiries/api_endpoints/Inquiry/Inquiry_Create/views.py
@@ -1,7 +1,7 @@
 from os import getenv
 
 from rest_framework.generics import CreateAPIView
-from telegram import Bot
+# from telegram import Bot
 
 from apps.users.models import TelegramUser
 from .serializers import InquiryCreateSerializer
@@ -12,9 +12,9 @@ class InquiryCreateAPIView(CreateAPIView):
 
     def perform_create(self, serializer):
         q = serializer.save()
-        bot = Bot(token=getenv('BOT_TOKEN'))
-        message = f"New Inquiry:\n\nName: {q.full_name}\nNumber: {q.phone_number}\nEmail: {q.email}"
-        for user in TelegramUser.objects.filter(active=True):
-            bot.send_message(chat_id=user.chat_id, text=message)
+        # bot = Bot(token=getenv('BOT_TOKEN'))
+        # message = f"New Inquiry:\n\nName: {q.full_name}\nNumber: {q.phone_number}\nEmail: {q.email}"
+        # for user in TelegramUser.objects.filter(active=True):
+        #     bot.send_message(chat_id=user.chat_id, text=message)
 
 # TODO: botga xabar jo'natadigan qismi celery orqali bo'lishi kerak

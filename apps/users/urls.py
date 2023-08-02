b8d10b9b2535b1dbdca5a0b4a428815c6d1ac0ee
BilolSolih <BilolMuhammadSolih@gmail.com>
2023-08-02 12:23:04 +0500

    added product id to the cart items

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path('update/', views.UserUpdateAPIView.as_view(), name='user_update'),
     path('delete/', views.UserDeleteAPIView.as_view(), name='user_delete'),
     path('like_product/<int:product_id>/', views.UserLikeProductAPIView.as_view(), name='user_like_product'),
+    path('like_product/list/', views.UserLikedProductsListAPIView.as_view(), name='user_liked_products_list'),
     path('change_password/', views.UserChangePasswordAPIView.as_view(), name='change_password'),
     path('reset_password/send_link/', views.UserPasswordResetSendLinkAPIView.as_view(), name='password_reset_send'),
     path('reset_password/reset/<str:uidb64>/<str:token>/', views.UserPasswordResetAPIView.as_view(), name='password_reset'),

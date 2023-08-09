bd9e8ff11252045b686263c14cb056eac2129dad
BilolSolih <BilolMuhammadSolih@gmail.com>
2023-08-09 15:53:17 +0500

    order create

--- apps/cart/api_endpoints/CartItem/CartItem_List/serializers.py
+++ apps/cart/api_endpoints/CartItem/CartItem_List/serializers.py
@@ -1,10 +1,18 @@
 from rest_framework import serializers
 
 from apps.cart.models import CartItem
+from apps.store.models.product import Product
+
+
+class ProductInCartItem(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'photo']
 
 
 class CartItemListSerializer(serializers.ModelSerializer):
+    product = ProductInCartItem(many=False)
+
     class Meta:
         model = CartItem
         fields = ['id', 'product', 'quantity', 'cost']
-        depth = 1

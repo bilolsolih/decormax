c86f91ea72513344200696bc50cbf9af6a76f73c
BilolSolih <BilolMuhammadSolih@gmail.com>
2023-08-10 22:08:04 +0500

    Hopefully final

--- apps/news/signals.py
+++ apps/news/signals.py
@@ -9,5 +9,5 @@ from apps.news.models import News, Picture
 @receiver(post_save, sender=News)
 def news_or_picture_saved(sender, instance, created, **kwargs):
     if created and instance.photo and instance.photo.width > 1920:
-        photo_compress.delay(instance.pk, instance._meta.app_label, instance._meta.model_name)
+        photo_compress(instance.pk, instance._meta.app_label, instance._meta.model_name)
 # TODO: In prod, compress must be called with delay

ebb812cee11b06e306ebdae5b8bde7971da6759d
BilolSolih <BilolMuhammadSolih@gmail.com>
2023-08-11 05:01:44 +0500

    Hopefully final

--- apps/cart/api_endpoints/CartItem/CartItem_Delete/views.py
+++ apps/cart/api_endpoints/CartItem/CartItem_Delete/views.py
@@ -1,6 +1,9 @@
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
+from rest_framework import status
 from rest_framework.generics import DestroyAPIView
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from apps.cart.models import CartItem
 
@@ -25,4 +28,27 @@ class CartItemDeleteAPIView(DestroyAPIView):
             return CartItem.objects.filter(device_id=device_id)
 
 
-__all__ = ['CartItemDeleteAPIView']
+class CartItemDeleteAllAPIView(APIView):
+    @swagger_auto_schema(
+        manual_parameters=[
+            openapi.Parameter('device_id', openapi.IN_QUERY, description='Device id', type=openapi.TYPE_STRING),
+        ]
+    )
+    def delete(self, request, *args, **kwargs):
+        query_set = self.get_queryset(request)
+        query_set.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    @staticmethod
+    def get_queryset(request):
+        user = request.user if request.user.is_authenticated else None
+        device_id = request.query_params.get('device_id', None)
+        if user and device_id:
+            raise ValueError('device_id is needed only for guest users.')
+        if user:
+            return CartItem.objects.filter(cart__user=user).delete()
+        else:
+            return CartItem.objects.filter(device_id=device_id).delete()
+
+
+__all__ = ['CartItemDeleteAPIView', 'CartItemDeleteAllAPIView']
--- apps/cart/urls.py
+++ apps/cart/urls.py
@@ -7,6 +7,7 @@ app_name = 'cart'
 urlpatterns = [
     path('cart_items/create/', views.CartItemCreateAPIView.as_view(), name='cart_item_create'),
     path('cart_items/list/', views.CartItemListAPIView.as_view(), name='cart_item_list'),
+    path('cart_items/delete/all/', views.CartItemDeleteAllAPIView.as_view(), name='cart_item_delete_all'),
     path('cart_items/delete/<int:pk>/', views.CartItemDeleteAPIView.as_view(), name='cart_item_delete'),
     path('cart_items/update/<int:pk>/', views.CartItemUpdateAPIView.as_view(), name='cart_item_update'),
 ]

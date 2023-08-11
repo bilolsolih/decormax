4c28c4693e6c46560ea72acebe37e6b9d06ea2f5
BilolSolih <BilolMuhammadSolih@gmail.com>
2023-08-11 05:21:36 +0500

    Hopefully final

--- apps/store/api_endpoints/Collection/Collection_List/serializers.py
+++ apps/store/api_endpoints/Collection/Collection_List/serializers.py
@@ -6,4 +6,4 @@ from apps.store.models.product import Collection
 class CollectionListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Collection
-        fields = ['id', 'title', 'photo', 'status']
+        fields = ['id', 'title', 'photo', 'price', 'status']

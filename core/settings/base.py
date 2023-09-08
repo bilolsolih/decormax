ee8d2d2b82a08d22085f38ff54bfd6714c5cbb1a
BilolSolih <BilolMuhammadSolih@gmail.com>
2023-09-08 15:45:18 +0500

    cors allowed origins

--- core/settings/base.py
+++ core/settings/base.py
@@ -144,9 +144,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"

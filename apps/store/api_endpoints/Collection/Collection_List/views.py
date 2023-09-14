dbf9e1b5604d7c546b054172b4393b7d2712c859
BilolSolih <BilolMuhammadSolih@gmail.com>
2023-09-14 17:59:58 +0500

    Pagination

--- apps/store/api_endpoints/Collection/Collection_List/views.py
+++ apps/store/api_endpoints/Collection/Collection_List/views.py
@@ -37,8 +37,8 @@ class CustomPageNumberPagination(PageNumberPagination):
             'count': self.page.paginator.count,
             'next': self.get_next_link(),
             'previous': self.get_previous_link(),
-            'results': data,
             'total_pages': self.page.paginator.num_pages,
+            'results': data,
         }, status=status.HTTP_200_OK)
 
 

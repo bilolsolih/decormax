f1d84be1d3d29b09e4cf9a35947e663848086bc9
BilolSolih <BilolMuhammadSolih@gmail.com>
2023-09-23 13:14:21 +0500

    fix: duplicate cart items are now identified based on articuls not the collections themselves

--- apps/store/models/store.py
+++ apps/store/models/store.py
@@ -5,12 +5,16 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 class Store(models.Model):
     title = models.CharField(verbose_name=_('Title'), max_length=128)
+    photo = models.ImageField(_('Store photo'), upload_to='images/store/stores/')
+    info = models.TextField(_('Info about the store'), blank=True, null=True)
     phone_number = PhoneNumberField(verbose_name=_('Phone number'), region='UZ')
     region = models.CharField(verbose_name=_('Region'), max_length=128)
     district = models.CharField(verbose_name=_('City or district'), max_length=128)
     address = models.CharField(verbose_name=_('Address'), max_length=256)
     orient = models.CharField(verbose_name=_('Orient'), max_length=256, blank=True, null=True)
     location = models.TextField(verbose_name=_('Location for iFrame'), blank=True, null=True)
+
+    is_certified = models.BooleanField(_('Is certified'), default=True)
 
     class Meta:
         verbose_name = _('Store')

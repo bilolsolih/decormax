7242419d3613894f1d5a70ca0a1a0742a37c0442
BilolSolih <BilolMuhammadSolih@gmail.com>
2023-09-23 13:21:03 +0500

    fix: duplicate cart items are now identified based on articuls not the collections themselves

--- apps/store/api_endpoints/Store/Store_List/serializers.py
+++ apps/store/api_endpoints/Store/Store_List/serializers.py
@@ -6,4 +6,4 @@ from apps.store.models.store import Store
 class StoreListSerializer(ModelSerializer):
     class Meta:
         model = Store
-        fields = ['id', 'title', 'phone_number', 'region', 'district', 'address', 'orient', 'location']
+        fields = ['id', 'title', 'photo', 'info', 'phone_number', 'region', 'district', 'address', 'orient', 'location', 'is_certified']

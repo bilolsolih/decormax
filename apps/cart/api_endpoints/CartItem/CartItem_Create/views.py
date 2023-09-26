508fe27f9c23382f54a8fa64e035ef04a88ebcfd
BilolSolih <BilolMuhammadSolih@gmail.com>
2023-09-26 19:05:53 +0500

    cart items are kept across registrations now

--- apps/cart/api_endpoints/CartItem/CartItem_Create/views.py
+++ apps/cart/api_endpoints/CartItem/CartItem_Create/views.py
@@ -1,8 +1,11 @@
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework.generics import CreateAPIView
 
 from .serializers import CartItemCreateSerializer
 
 
+@method_decorator(csrf_exempt, name='post')
 class CartItemCreateAPIView(CreateAPIView):
     serializer_class = CartItemCreateSerializer
 

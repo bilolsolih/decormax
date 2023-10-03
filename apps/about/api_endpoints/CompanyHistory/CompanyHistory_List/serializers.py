f49f53bad508dad4787958592e1f2b54b5842f4a
Shakhabutdinov <uabdulaz038@gmail.com>
2023-10-03 14:16:09 +0500

    serializers.py updated

--- apps/about/api_endpoints/CompanyHistory/CompanyHistory_List/serializers.py
+++ apps/about/api_endpoints/CompanyHistory/CompanyHistory_List/serializers.py
@@ -6,4 +6,4 @@ from apps.about.models import CompanyHistory
 class CompanyHistoryListSerializer(ModelSerializer):
     class Meta:
         model = CompanyHistory
-        fields = ['id', 'year', 'content']
+        fields = '__all__'

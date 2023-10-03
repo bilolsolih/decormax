258c0ad7b919618648b2e18932f4c64841f2f114
BilolSolih <BilolMuhammadSolih@gmail.com>
2023-10-03 15:41:53 +0500

    cart items are kept across registrations now

--- apps/store/api_endpoints/Collection/Collection_List/views.py
+++ apps/store/api_endpoints/Collection/Collection_List/views.py
@@ -4,6 +4,7 @@ from rest_framework import status
 from rest_framework.generics import ListAPIView
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
+from django.db.models import Q
 
 from apps.store.models.product import Collection
 from apps.store.models.product_parameters import TargetRoom, Style, PictureType, Color, Size
@@ -11,13 +12,11 @@ from .serializers import CollectionListSerializer
 
 
 class CollectionFilterSet(FilterSet):
-    title = django_filters.CharFilter(field_name='title', lookup_expr='icontains')
     target_room = django_filters.ModelMultipleChoiceFilter(field_name='target_room__pk', queryset=TargetRoom.objects.all(), to_field_name='pk')
     style = django_filters.ModelMultipleChoiceFilter(field_name='style__pk', queryset=Style.objects.all(), to_field_name='pk')
     picture_type = django_filters.ModelMultipleChoiceFilter(field_name='picture_type__pk', queryset=PictureType.objects.all(), to_field_name='pk')
     color = django_filters.ModelMultipleChoiceFilter(field_name='color__pk', queryset=Color.objects.all(), to_field_name='pk')
     size = django_filters.ModelMultipleChoiceFilter(field_name='size__pk', queryset=Size.objects.all(), to_field_name='pk')
-    articul = django_filters.CharFilter(field_name='articuls__title', lookup_expr='iexact')
 
     class Meta:
         model = Collection
@@ -36,10 +35,15 @@ class CustomPageNumberPagination(PageNumberPagination):
 
 
 class CollectionListAPIView(ListAPIView):
-    queryset = Collection.objects.all()
     serializer_class = CollectionListSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_class = CollectionFilterSet
 
+    def get_queryset(self):
+        queryset = Collection.objects.all()
+        s = self.request.query_params.get('s', None)
+        if s:
+            queryset = queryset.filter(Q(title__icontains=s) | Q(articuls__title__iexact=s))
+        return queryset
 
-__all__ = ['CollectionListAPIView']
+    __all__ = ['CollectionListAPIView']

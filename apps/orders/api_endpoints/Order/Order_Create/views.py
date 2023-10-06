4db23dd2ecf11762dc7c4f96e756a587079f499a
Shakhabutdinov <uabdulaz038@gmail.com>
2023-10-06 10:05:26 +0500

    views.py updated

--- apps/orders/api_endpoints/Order/Order_Create/views.py
+++ apps/orders/api_endpoints/Order/Order_Create/views.py
@@ -1,4 +1,3 @@
-import requests
 from django.db.models.aggregates import Sum
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
@@ -34,37 +33,10 @@ class OrderCreateAPIView(CreateAPIView):
 
         final_price = items.aggregate(final_price=Sum('cost'))['final_price']
         order = serializer.save(user=user, final_price=final_price)
-        photos = []
         for item in items:
             OrderItem.objects.create(order=order, collection=item.collection, artikul=item.articul,
                                      quantity=item.quantity, cost=item.cost)
-            photos.append(('photo', open(item.articul.photo.path, 'rb')))
             item.delete()
-
-        telegram_bot_token = '6619661511:AAFbb2HydLQNVdIqkzh6slLUsxWvKM0xxQI'
-        chat_id = '-1001920070201'
-
-        message = f"Ф.И.О: {order.full_name}\n"
-        message += f"Номер телефона: {order.phone_number}\n"
-        message += f"Email: {order.email}\n"
-        message += f"Магазин: {order.store}\n"
-        message += f"Тип доставки: {order.delivery_type}\n"
-        message += f"Способ оплаты: {order.payment_method}\n"
-        message += f"Город: {order.city}\n"
-        message += f"Регион: {order.region}\n"
-        message += f"Адрес: {order.address}\n"
-        message += f"Этаж: {order.level}\n"
-        message += f"Дата доставки: {order.delivery_date}\n"
-
-        response = requests.post(
-            f"https://api.telegram.org/bot{telegram_bot_token}/sendMediaGroup",
-            data={'chat_id': chat_id},
-            files=photos,
-            params={'caption': message}
-        )
-
-        if response.status_code != 200:
-            pass
 
 
 __all__ = ['OrderCreateAPIView']

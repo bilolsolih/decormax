574fc7f32752b38d6afcb51391291d22965b00d9
BilolSolih <BilolMuhammadSolih@gmail.com>
2023-10-09 11:51:53 +0500

    updated migrations

--- apps/store/api_endpoints/Articul/Articul_List/serializers.py
+++ apps/store/api_endpoints/Articul/Articul_List/serializers.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
 
-from apps.store.models.product import Articul
+from apps.store.models.product import Articul, Collection
+
+
+class CollectionInArticulSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ['id', 'title']
 
 
 class ArticulListSerializer(serializers.ModelSerializer):
+    collection = CollectionInArticulSerializer(many=False)
+
     class Meta:
         model = Articul
-        fields = ['id', 'title', 'photo']
+        fields = ['id', 'title', 'photo', 'collection']
--- apps/store/api_endpoints/Articul/Articul_List/views.py
+++ apps/store/api_endpoints/Articul/Articul_List/views.py
@@ -1,13 +1,18 @@
 from rest_framework.generics import ListAPIView
 
 from apps.store.models.product import Articul
-
 from .serializers import ArticulListSerializer
 
 
 class ArticulListAPIView(ListAPIView):
     serializer_class = ArticulListSerializer
-    queryset = Articul.objects.all()
+
+    def get_queryset(self):
+        queryset = Articul.objects.all()
+        s = self.request.query_params.get('s', None)
+        if s:
+            queryset = queryset.filter(title__icontains=s)
+        return queryset
 
 
 __all__ = ['ArticulListAPIView']
